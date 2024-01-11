c9352e1ca3cfa3af29691ee3505249a73c7b8fcf
RK27-OPS <124685233+RK27-OPS@users.noreply.github.com>
2024-01-11 14:59:29 +0200

    5.

--- vienadiatteli.py
+++ vienadiatteli.py
@@ -1,24 +1,27 @@
+from cgitb import reset
 from tkinter import *
 from PIL import ImageTk, Image
 import random
 from tkinter import messagebox
+
 gamewindow = Tk()
 gamewindow.title("Vienādie attēli")
 gamewindow.configure(bg="black")
 gamewindow.resizable(1,1)
-
-count = 0
-correctAnswer = 0
-answers = []
-answer_dict = {}
 
 myImg1 = ImageTk.PhotoImage(Image.open("1.jpg"))
 myImg2 = ImageTk.PhotoImage(Image.open("2.jpg"))
 myImg3 = ImageTk.PhotoImage(Image.open("3.jpg"))
 myImg4 = ImageTk.PhotoImage(Image.open("4.jpg"))
 myImg5 = ImageTk.PhotoImage(Image.open("5.jpg"))
-#myImg6 = ImageTk.PhotoImage(Image.open("6.jpg"))
+myImg6 = ImageTk.PhotoImage(Image.open("6.jpg")
 bgImg = ImageTk.PhotoImage(Image.open("7.jpg"))
+
+ImageList = [myImg1, myImg1, myImg2, myImg2, myImg3, myImg3, myImg4, myImg4, myImg5, myImg5, ]
+myLabel=Label(image=myImg1)
+
+bgImg= ImageTk
+
 
 def btnClick(btn, number):
     global count, correctAnswers, answers, answer_dict
@@ -46,21 +49,25 @@ def btnClick(btn, number):
     answer_dict = {}
     return 0
 
+def infoLogs():
+    gamewindow=Toplevel()
+    gamewindow.title("Info par programmu")
+    gamewindow.geometry("500x300")
+    apraksts=Label(gamewindow,text=)
 
+    return 0
 
-ImageList = [myImg1, myImg1, myImg2, myImg2, myImg3, myImg3, myImg4, myImg4, myImg5, myImg5]
-random.shuffle(ImageList)
 
 btn0=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn0, 0))
-btn1=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn1, 0))
-btn2=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn2, 0))
-btn3=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn3, 0))
-btn4=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn4, 0))
-btn5=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn5, 0))
-btn6=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn6, 0))
-btn7=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn7, 0))
-btn8=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn8, 0))
-btn9=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn9, 0))
+btn1=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn1, 1))
+btn2=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn2, 2))
+btn3=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn3, 3))
+btn4=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn4, 4))
+btn5=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn5, 5))
+btn6=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn6, 6))
+btn7=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn7, 7))
+btn8=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn8, 8))
+btn9=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn9, 9))
 
 btn0.grid(row = 0, column = 0)
 btn1.grid(row = 0, column = 1)
@@ -73,11 +80,22 @@ btn7.grid(row = 1, column = 2)
 btn8.grid(row = 1, column = 3)
 btn9.grid(row = 1, column = 4)
 
+random.shuffle(ImageList)
+
+count = 0
+correctAnswer = 0
+answers = []
+answer_dict = {}
+answerCount=0
+
+def reset():
+    global count, corectAnswewr
+
 galvenaIzvele=Menu(gamewindow)
-gamewindow.confing(menu=galvenaIzvele)
+gamewindow.config(menu=galvenaIzvele)
 
 opcijas=Menu(galvenaIzvele,tearoff=False)
-galvenaIzvele.add_command(label="Opcijas",menu=opcijas)
+galvenaIzvele.add_cascade(label="Opcijas",menu=opcijas)
 
 #opciajs.add_command(label="Jauna spēle",command=reset)
 #opciajs.add_command(label="Iziet",command=gamewindow.quit)
