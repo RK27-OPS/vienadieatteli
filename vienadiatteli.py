3622ced2509717a4020463b5f3206f066d03a79e
RK27-OPS <124685233+RK27-OPS@users.noreply.github.com>
2024-01-16 13:33:21 +0200

    Update vienadiatteli.py

--- vienadiatteli.py
+++ vienadiatteli.py
@@ -14,20 +14,20 @@ myImg2 = ImageTk.PhotoImage(Image.open("2.jpg"))
 myImg3 = ImageTk.PhotoImage(Image.open("3.jpg"))
 myImg4 = ImageTk.PhotoImage(Image.open("4.jpg"))
 myImg5 = ImageTk.PhotoImage(Image.open("5.jpg"))
-myImg6 = ImageTk.PhotoImage(Image.open("6.jpg")
+myImg6 = ImageTk.PhotoImage(Image.open("6.jpg"))
 bgImg = ImageTk.PhotoImage(Image.open("7.jpg"))
 
-ImageList = [myImg1, myImg1, myImg2, myImg2, myImg3, myImg3, myImg4, myImg4, myImg5, myImg5, ]
+ImageList = [myImg1, myImg1, myImg2, myImg2, myImg3, myImg3, myImg4, myImg4, myImg5, myImg5,myImg6, myImg6 ]
 myLabel=Label(image=myImg1)
 
 bgImg= ImageTk
 
 
-def btnClick(btn, number):
-    global count, correctAnswers, answers, answer_dict
+def btnClick(btn, number):# pogu funkcija lai klikšķinātu
+    global count, correctAnswer, answers, answer_dict
     if btn["image"] == "pyImage7" and count < 2:
         btn["image"] = ImageList[number]
-        count +- 1
+        count += 1
         answers.append(number)
         answer_dict[btn] = ImageList(number)
     if len(answers) == 2:
@@ -49,36 +49,77 @@ def btnClick(btn, number):
     answer_dict = {}
     return 0
 
-def infoLogs():
+def infoLogs():# info logs
     gamewindow=Toplevel()
     gamewindow.title("Info par programmu")
     gamewindow.geometry("500x300")
-    apraksts=Label(gamewindow,text=)
+    apraksts=Label(gamewindow,text="Atmini 2 vienādus attēlus")
 
     return 0
 
+#pogas
+btn0=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn0, 0))
+btn1=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn1, 1))
+btn2=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn2, 2))
+btn3=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn3, 3))
+btn4=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn4, 4))
+btn5=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn5, 5))
+btn6=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn6, 6))
+btn7=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn7, 7))
+btn8=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn8, 8))
+btn9=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn9, 9))
+btn10=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn10, 10))
+btn11=Button(width=200,height=300, image = bgImg, command = lambda: btnClick(btn11, 11))
 
-btn0=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn0, 0))
-btn1=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn1, 1))
-btn2=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn2, 2))
-btn3=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn3, 3))
-btn4=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn4, 4))
-btn5=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn5, 5))
-btn6=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn6, 6))
-btn7=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn7, 7))
-btn8=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn8, 8))
-btn9=Button(width = 200, height = 300, image = bgImg, command = lambda: btnClick(btn9, 9))
 
-btn0.grid(row = 0, column = 0)
-btn1.grid(row = 0, column = 1)
-btn2.grid(row = 0, column = 2)
-btn3.grid(row = 0, column = 3)
-btn4.grid(row = 0, column = 4)
-btn5.grid(row = 1, column = 0)
-btn6.grid(row = 1, column = 1)
-btn7.grid(row = 1, column = 2)
-btn8.grid(row = 1, column = 3)
-btn9.grid(row = 1, column = 4)
+
+#pogu novietojums
+btn0.grid(row = 1, column = 1)
+btn1.grid(row = 2, column = 2)
+btn2.grid(row = 3, column = 3)
+btn3.grid(row = 1, column = 4)
+btn4.grid(row = 2, column = 1)
+btn5.grid(row = 3, column = 2)
+btn6.grid(row = 1, column = 3)
+btn7.grid(row = 2, column = 4)
+btn8.grid(row = 3, column = 1)
+btn9.grid(row = 1, column = 2)
+btn10.grid(row = 2, column = 3)
+btn11.grid(row = 3, column = 4)
+
+
+
+def reset():
+    global count, correctAnswer, answers, answer_dict, answerCount
+    btn0.config(state=NORMAL)
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn10.config(state=NORMAL)
+    btn11.config(state=NORMAL)
+
+
+    btn0["image"]="pyimage7"#bg attēls virs 
+    btn1["image"]="pyimage7"
+    btn2["image"]="pyimage7"
+    btn3["image"]="pyimage7"
+    btn4["image"]="pyimage7"
+    btn5["image"]="pyimage7"
+    btn6["image"]="pyimage7"
+    btn7["image"]="pyimage7"
+    btn8["image"]="pyimage7"
+    btn9["image"]="pyimage7"
+    btn10["image"]="pyimage7"
+    btn11["image"]="pyimage7"
+
+    return 0
+
 
 random.shuffle(ImageList)
 
@@ -87,9 +128,6 @@ correctAnswer = 0
 answers = []
 answer_dict = {}
 answerCount=0
-
-def reset():
-    global count, corectAnswewr
 
 galvenaIzvele=Menu(gamewindow)
 gamewindow.config(menu=galvenaIzvele)
